2c9b88a7966bca74317071cec2e9d50977a1f89f
Antti Karjalainen <antti.karjalainen@develore.com>
2023-01-03 17:23:21 +0200

    Exclude videos folder, add videoplayer component playing 1 video from public folder

--- pianocoverclient/src/App.tsx
+++ pianocoverclient/src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import { VideoPlayer } from "./ReactPlayer/VideoPlayer";
 import { SideBar } from "./Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="text-center">
       <SideBar></SideBar>
+      <div className="align-middle">
+        <VideoPlayer></VideoPlayer>
+      </div>
     </div>
   );
 }
