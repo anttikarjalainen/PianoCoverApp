08308f285af2162ada73e45718217adce628a5d1
Antti Karjalainen <antti.karjalainen@develore.com>
2023-01-03 16:57:22 +0200

    Add basic sidebar

--- pianocoverclient/src/App.tsx
+++ pianocoverclient/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { SideBar } from "./Sidebar/Sidebar";
 
 function App() {
-  return <div className="text-center">kontsa</div>;
+  return (
+    <div className="text-center">
+      <SideBar></SideBar>
+    </div>
+  );
 }
 
 export default App;
